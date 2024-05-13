75a40801f12d78937b03a5a07f3a90081647d0bc
abdulrahmannouh <abdulrahman.nouh20@gmail.com>
2024-05-13 17:46:16 +0300

    intro_point

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+class ConstVariebles {
+  String bear_img_url =
+      'https://s3-alpha-sig.figma.com/img/8fa0/c2de/def109dcb822b678344e486976f8d43a?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m6JEO9tgMDfL5UvVfMtERTeWxCNy1nNTrhcbGdyp-0pUi6if2cv2nfZ~5Ox1mc8S2JEmYAeD0GlMrwTeQgYeiD8GnwfnyZ756M-sg9E1IkqsLPDAdlDmIbfXkn0lOIg5mp5I3scrvTYnv9pNcHaJP1oWoABTCSuNtetodWBaOHgxwVAFBrH3gK7ygXRt3T113lKMF7-sRg3ELNgqGH6-Q1uGazdi0lGFzNKT~RMi6YI8k5q3Lo4c0tEaej7Rcvz~e-Ngrdh~s5mwG1TSt73oTDRUCpwdPvnmXQo7oCtyf77dDk9BLLBVCceyyU6Qex~okKPBd9mMZeXH7oyuzEBxsw__';
+  ConstVariebles() {
+    bear_img_url;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:osscam_app/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isvisible = true;
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: const Color.fromRGBO(10, 50, 77, 1),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 189.0,
+                left: 36.0,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 450.0,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(217, 217, 217, 1),
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Container(
+                            margin:
+                                const EdgeInsets.only(top: 67, right: 150.0),
+                            child: const Text(
+                              "First name",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10),
+                            color: Colors.transparent,
+                            width: 226,
+                            height: 43,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Your firstname',
+                                  hintStyle: const TextStyle(
+                                      color: Color.fromRGBO(27, 47, 108, 1),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400),
+                                  filled: true,
+                                  fillColor:
+                                      const Color.fromRGBO(119, 193, 193, 0.75),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          width: 1.0,
+                                          color: Color.fromRGBO(10, 50, 77, 1)),
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                          Container(
+                            margin:
+                                const EdgeInsets.only(top: 10, right: 170.0),
+                            child: const Text(
+                              "Email",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10),
+                            color: Colors.transparent,
+                            width: 226,
+                            height: 43,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: 'Your Email',
+                                  hintStyle: const TextStyle(
+                                      color: Color.fromRGBO(27, 47, 108, 1),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400),
+                                  filled: true,
+                                  fillColor:
+                                      const Color.fromRGBO(119, 193, 193, 0.75),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          width: 1.0,
+                                          color: Color.fromRGBO(10, 50, 77, 1)),
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          ),
+                          Container(
+                            margin:
+                                const EdgeInsets.only(top: 10, right: 150.0),
+                            child: const Text(
+                              "Password",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 10),
+                            color: Colors.transparent,
+                            width: 226,
+                            height: 43,
+                            child: TextField(
+                              obscureText: _isvisible,
+                              decoration: InputDecoration(
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      _isvisible
+                                          ? Icons.visibility
+                                          : Icons.visibility_off,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _isvisible != _isvisible;
+                                      });
+                                    },
+                                  ),
+                                  hintText: 'Your Password',
+                                  hintStyle: const TextStyle(
+                                      color: Color.fromRGBO(27, 47, 108, 1),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400),
+                                  filled: true,
+                                  fillColor:
+                                      const Color.fromRGBO(119, 193, 193, 0.75),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide: const BorderSide(
+                                          width: 1.0,
+                                          color: Color.fromRGBO(10, 50, 77, 1)),
+                                      borderRadius: BorderRadius.circular(10))),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 115,
+                left: 120.0,
+                child: Container(
+                  width: 120.0,
+                  height: 120.0,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 6),
+                      shape: BoxShape.circle,
+                      color: Colors.blue.shade200),
+                ),
+              ),
+              Positioned(
+                top: 135,
+                left: 130.0,
+                child: Container(
+                  width: 100.0,
+                  height: 100.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(ConstVariebles().bear_img_url),
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
